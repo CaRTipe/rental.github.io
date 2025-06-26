992599a3bc17acf1b909130bbed14b295cc5d53c
CaRTipe <blairmbugua868@gmail.com>
2025-06-26 11:25:03 +0300

    Implement code changes to enhance functionality and improve performance

--- admin/includes/navbar.php
+++ admin/includes/navbar.php
@@ -1,9 +1,10 @@
 <nav class="navbar navbar-expand-lg bg-body-tertiary mb-5" id="navbar">
-    <div class="container d-flex flex-row align-items-center justify-content-between">
+    <div class="container d-flex align-items-center justify-content-between">
+
         <!-- Brand -->
-        <div id="brand" class="d-flex align-items-center">
-            <img src="./assets/images/house.png" alt="" width="40px" height="40px">
-            <a class="navbar-brand ms-2" href="#">Sustainable Houses</a>
+        <div class="d-flex align-items-center">
+            <img src="./assets/images/house.png" alt="" width="40" height="40">
+            <a class="navbar-brand ms-2 mb-0 h1" href="#">Sustainable Houses</a>
         </div>
 
         <!-- Offcanvas Toggle Button -->
@@ -19,7 +20,7 @@
                 <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
             </div>
             <div class="offcanvas-body">
-                <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                <ul class="navbar-nav d-flex align-items-center flex-grow-1 pe-3">
                     <li class="nav-item mx-2">
                         <a class="nav-link" href="index.php">Houses</a>
                     </li>
@@ -30,7 +31,9 @@
                         <a class="nav-link" href="clients.php">Clients</a>
                     </li>
                 </ul>
-                <div class="mt-3">
+
+                <!-- Log out Button -->
+                <div class="d-lg-none mt-3">
                     <?php if (isset($_SESSION['login']) && $_SESSION['login']): ?>
                         <a href="../logout.php">
                             <button class="btn btn-secondary w-100">Log out</button>
@@ -39,5 +42,15 @@
                 </div>
             </div>
         </div>
+
+        <!-- Log out Button for large screens -->
+        <div class="d-none d-lg-block ms-3">
+            <?php if (isset($_SESSION['login']) && $_SESSION['login']): ?>
+                <a href="../logout.php">
+                    <button class="btn btn-secondary" style="width: 150px; height: 45px;">Log out</button>
+                </a>
+            <?php endif; ?>
+        </div>
+
     </div>
 </nav>
--- navbar/navbar.php
+++ navbar/navbar.php
@@ -1,9 +1,16 @@
+<?php
+// Detect current page
+$currentPage = basename($_SERVER['PHP_SELF']);
+
+// Check if it's signin or signup page
+$isAuthPage = in_array($currentPage, ['signin.php', 'signup.php', 'agentin.php', 'agentup.php']);
+?>
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
-    <div class="container">
+    <div class="container d-flex align-items-center justify-content-between">
         <!-- Brand -->
         <a class="navbar-brand d-flex align-items-center gap-2" href="#">
             <img src="./assets/images/house.png" alt="" width="40" height="40">
-            Sustainable Houses
+            <span class="fw-bold">Sustainable Houses</span>
         </a>
 
         <!-- Toggler for Offcanvas -->
@@ -17,32 +24,33 @@
             aria-labelledby="offcanvasNavbarLabel">
             <div class="offcanvas-header">
                 <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Menu</h5>
-                <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas"
+                    aria-label="Close"></button>
             </div>
-            <div class="offcanvas-body">
+            <div class="offcanvas-body d-flex flex-column flex-lg-row align-items-lg-center justify-content-between w-100">
                 <!-- Nav links -->
-                <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-                    <li class="nav-item">
-                        <a class="nav-link" href="./index.php">Home</a>
+                <ul class="navbar-nav justify-content-end flex-grow-1 pe-3 flex-lg-row">
+                    <li class="nav-item px-lg-2">
+                        <a class="nav-link <?= $isAuthPage ? 'text-dark' : 'text-white' ?>" href="./index.php">Home</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">About Us</a>
+                    <li class="nav-item px-lg-2">
+                        <a class="nav-link <?= $isAuthPage ? 'text-dark' : 'text-white' ?>" href="#">About Us</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="propertydetails.php">Properties</a>
+                    <li class="nav-item px-lg-2">
+                        <a class="nav-link <?= $isAuthPage ? 'text-dark' : 'text-white' ?>" href="propertydetails.php">Properties</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Services</a>
+                    <li class="nav-item px-lg-2">
+                        <a class="nav-link <?= $isAuthPage ? 'text-dark' : 'text-white' ?>" href="#">Services</a>
                     </li>
                 </ul>
 
                 <!-- Login/Sign Up Buttons -->
-                <div class="mt-3 d-flex gap-2">
+                <div class="mt-3 mt-lg-0 d-flex gap-2">
                     <a href="./signin.php">
-                        <button class="btn btn-outline-light w-100">Login</button>
+                        <button class="btn btn-outline-light btn-sm btn-lg-outline-dark">Login</button>
                     </a>
                     <a href="./signup.php">
-                        <button class="btn btn-light w-100">Sign Up</button>
+                        <button class="btn btn-light btn-sm btn-lg-dark">Sign Up</button>
                     </a>
                 </div>
             </div>
